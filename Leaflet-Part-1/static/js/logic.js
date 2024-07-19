b7224c3914492db96a8f49b58cfcff92541a4450
Zach_Mason <zmason04@gmail.com>
2024-07-19 15:46:06 -0400

    updates to code and readme

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -12,12 +12,13 @@ function markerSize(magnitude) {
 
 // Function to determine marker color based on depth
 function markerColor(depth) {
-    return depth > 90 ? 'darkred' :
-        depth > 70 ? 'orangered' :
-        depth > 50 ? 'orange' :
-        depth > 30 ? 'yellow' :
-        depth > 10 ? 'lightgreen' :
-                        'darkgreen';
+    let colors = ['#00ff00', '#ccff00', '#ffff00', '#ffd700', '#ffa500', '#ff0000'];
+    if (depth < 10) return colors[0];
+    else if (depth < 30) return colors[1];
+    else if (depth < 50) return colors[2];
+    else if (depth < 70) return colors[3];
+    else if (depth < 90) return colors[4];
+    else return colors[5];
 }
 
 // Use this link to get the GeoJSON data (all from past 7 days).
@@ -45,14 +46,20 @@ d3.json(link).then(function(data) {
 // Set up the legend.
 let legend = L.control({position: "bottomright"});
 legend.onAdd = function() {
-let depthRanges = [
-    {limit: 0, color: 'darkgreen', label: "<10 km"},
-    {limit: 10, color: 'lightgreen', label: "10-30 km"},
-    {limit: 30, color: 'yellow', label: "30-50 km"},
-    {limit: 50, color: 'orange', label: "50-70 km"},
-    {limit: 70, color: 'orangered', label: "70-90 km"},
-    {limit: 90, color: 'darkred', label: ">90 km"}
-];
-}
+    let div = L.DomUtil.create("div", "info legend");
+    div.style.backgroundColor = "white"; // background color
+    div.style.padding = "10px";
+    let labels = [0, 10, 30, 50, 70, 90];
+    let colors = ['#00ff00', '#ccff00', '#ffff00', '#ffd700', '#ffa500', '#ff0000'];
 
+// Loop through depthRanges and generate the HTML for each color/label
+for (let i = 0; i < labels.length; i++) {
+    div.innerHTML +=
+        '<i style="background-color:' + colors[i] + '; width: 18px; height: 18px; display: inline-block; margin-right: 5px;"></i> ' +
+        labels[i] + (labels[i + 1] ? '&ndash;' + labels[i + 1] + ' km<br>' : '+ km');
+}
+return div;
+};
+
+// adding legend to map
 legend.addTo(quakeMap);
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leaflet-challenge
 Module 15 Challenge
+Background
+The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
+
+The USGS is interested in building a new set of tools that will allow them to visualize their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. In this challenge, you have been tasked with developing a way to visualize USGS data that will allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
+
+To do this, I chose a dataset from the USGS that included all earthquakes recorded within the past 7 days.  Each earthquake was mapped out based on the coordinates where they were recorded.  The magnitude of the earthquake is represented by size and depth is indicated by color.  Meaning, the greater the magnitude the greater the marker size, and the darker the marker color, the greater the depth.
